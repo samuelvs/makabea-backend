937bbc82d9dba289a21811515e6baa743001aefe
samuelvs <samuelveloso42@gmail.com>
2020-01-20 23:03:05 -0300

    Adjustment routers

--- start/routes.js
+++ start/routes.js
@@ -25,4 +25,36 @@ Route.group(() => {
     Route.get('/:id', 'UserController.show')
     Route.put('/:id', 'UserController.update')
     Route.delete('/:id', 'UserController.destroy')
-  }).prefix('/users')
+}).prefix('/users').middleware(['auth']);
+
+Route.group(() => {
+    Route.get('/', 'AdressController.index')
+    Route.post('/', 'AdressController.store')
+    Route.get('/:id', 'AdressController.show')
+    Route.put('/:id', 'AdressController.update')
+    Route.delete('/:id', 'AdressController.destroy')
+}).prefix('/address').middleware(['auth']);
+
+Route.group(() => {
+    Route.get('/', 'DangerSignalController.index')
+    Route.post('/', 'DangerSignalController.store')
+    Route.get('/:id', 'DangerSignalController.show')
+    Route.put('/:id', 'DangerSignalController.update')
+    Route.delete('/:id', 'DangerSignalController.destroy')
+}).prefix('/danger-signal').middleware(['auth'])
+
+Route.group(() => {
+    Route.get('/', 'GratefulReasonController.index')
+    Route.post('/', 'GratefulReasonController.store')
+    Route.get('/:id', 'GratefulReasonController.show')
+    Route.put('/:id', 'GratefulReasonController.update')
+    Route.delete('/:id', 'GratefulReasonController.destroy')
+}).prefix('/grateful-reason').middleware(['auth']);
+
+Route.group(() => {
+    Route.get('/', 'EmotionalStateController.index')
+    Route.post('/', 'EmotionalStateController.store')
+    Route.get('/:id', 'EmotionalStateController.show')
+    Route.put('/:id', 'EmotionalStateController.update')
+    Route.delete('/:id', 'EmotionalStateController.destroy')
+}).prefix('/emotional-state').middleware(['auth']);
