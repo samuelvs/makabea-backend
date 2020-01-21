462f0399a28ecf5b061ba9620a34aa7aaa7a358f
samuelvs <samuelveloso42@gmail.com>
2020-01-20 23:02:50 -0300

    Adjustment address model

--- app/Models/Address.js
+++ app/Models/Address.js
@@ -3,7 +3,7 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Adress extends Model {
+class Address extends Model {
 }
 
-module.exports = Adress
+module.exports = Address
