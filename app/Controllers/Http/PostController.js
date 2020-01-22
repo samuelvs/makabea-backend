745f7e00628faa75c4b9202516e33f49012c8183
samuelvs <samuelveloso42@gmail.com>
2020-01-21 22:31:25 -0300

    Added posts

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,91 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const PostModel = use('App/Models/Post');
+/**
+ * Resourceful controller for interacting with posts
+ */
+class PostController {
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response }) {
+    const post = PostModel.all();
+
+    return post;
+  }
+
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    const data = request.only(["title", "description", "author"]);
+
+    const post = await PostModel.create(data);
+    return post;
+  }
+
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params }) {
+    const post = await PostModel.findOrFail(params.id);
+
+    return post;
+  }
+
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request }) {
+    const post = await PostModel.findOrFail(params.id);
+    const data = request.only(["title", "description", "author"]);
+
+    post.merge(data);
+    await post.save();
+
+    return post;
+  }
+
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params }) {
+    const post = await PostModel.findOrFail(params.id);
+    await post.delete();
+
+    return post;
+  }
+}
+
+module.exports = PostController
